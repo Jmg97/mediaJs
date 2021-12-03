db4c9898c553f6d06ebe0629c93996ddb17fb7c7
Jmg97 <carasj78@naver.com>
2021-12-03 17:36:48 +0900

    1page

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0"); // .padstart(n, 'str') n의 자리로 만들고 그 자리에 str을 채우기
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
