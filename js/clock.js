f77c717f637554cd45ad28bc5a4c97dafc5dd297
Jmg97 <92910931+Jmg97@users.noreply.github.com>
2021-12-30 14:23:01 +0900

    time

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,14 @@
 const clock = document.querySelector("#clock");
+const day = document.querySelector(".day");
+const month = document.getElementById("month");
+const week = document.getElementById("week");
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+
+const days = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
+
+
 
 function getClock() {
   const date = new Date();
@@ -8,5 +18,32 @@ function getClock() {
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+function time() {
+  const weekDate = new Date();
+  const getHour = String(weekDate.getHours()).padStart(2, "0");
+  const getMinute = String(weekDate.getMinutes()).padStart(2, "0");
+  const getSecond = String(weekDate.getSeconds()).padStart(2, "0");
+  const getMonth = String(weekDate.getMonth() +1).padStart(2, "0");
+  const getDate = String(weekDate.getDate()).padStart(2, "0");
+  const getDays = String(weekDate.getDay());
+ 
+
+  hour.innerText = `${getHour}`;
+  minute.innerText = `${getMinute}`;
+  second.innerText = `${getSecond}`
+  month.innerText = `${getMonth}`;
+  week.innerText = `${getDate}`;
+}
+
+function getDate(){
+  const dayOfWeek = days[new Date().getDay()];
+  return dayOfWeek
+}
+
+
 getClock();
+getDate();
+time();
+setInterval(getDate, 1000);
+setInterval(time, 1000);
 setInterval(getClock, 1000);
