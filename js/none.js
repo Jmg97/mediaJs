9fd78926eea03e6236eb30a42603ed96dfe4b38b
Jmg97 <92910931+Jmg97@users.noreply.github.com>
2021-12-30 14:32:51 +0900

    margin-font

--- js/none.js
+++ js/none.js
@@ -2,6 +2,7 @@ const transparentBtn = document.querySelector("#transparent-all");
 const main = document.querySelector("#all-elements");
 const resetTransparentBtn = document.querySelector("#reset-trans-btn");
 const colorBox = document.querySelector(".hidden-box");
+const watchBox = document.querySelector(".watch-box");
 
 
 
@@ -12,6 +13,7 @@ const FLEX = "flex"
 function handleClickTransparent(event) {
   event.preventDefault();
   main.style.display = NONE;
+  watchBox.style.display = FLEX;
   resetTransparentBtn.style.display = BLOCK;
   colorBox.style.display = 'flex'
 }
@@ -21,6 +23,8 @@ transparentBtn.addEventListener("click", handleClickTransparent);
 function handleClickResetTransparent(event) {
   event.preventDefault();
   main.style.display = FLEX;
+  
+  watchBox.style.display = NONE;
   resetTransparentBtn.style.display = NONE;
   colorBox.style.display = 'none'
 }
