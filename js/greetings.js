ddf61f3cbdba6f7eda4140fd55c6a2927a44d919
Jmg97 <carasj78@naver.com>
2022-01-03 14:00:42 +0900

    weather

--- js/greetings.js
+++ js/greetings.js
@@ -1,5 +1,6 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-form :nth-child(1)");
+const logOut = document.querySelector("#login-form :nth-child(3)");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
@@ -16,6 +17,7 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Welcome ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  logOut.classList.remove(HIDDEN_CLASSNAME)
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
